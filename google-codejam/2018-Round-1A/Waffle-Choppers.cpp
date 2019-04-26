4a086bbaf05b002cb57cab6e72a391ee0e346239
Kairui Song <kasong@redhat.com>
2019-04-25 20:20:00 -0400

    Update 2018-1A

--- google-codejam/2018-Round-1A/Waffle-Choppers.cpp
+++ google-codejam/2018-Round-1A/Waffle-Choppers.cpp
@@ -1,28 +1,32 @@
 /*
  * Author: Kairui Song
- * Date: 2018-04-14
+ * Date: 2019-04-22
  */
-#include <iomanip>
 #include <iostream>
+#include <limits>
+#include <iomanip>
 #include <algorithm>
+#include <cmath>
+#include <list>
+#include <map>
+#include <set>
 #include <array>
+#include <queue>
+#include <vector>
 #include <bitset>
 #include <cstring>
 #include <forward_list>
-#include <limits>
-#include <list>
-#include <map>
-#include <queue>
+#include <unordered_set>
 #include <unordered_map>
-#include <vector>
-#include <cmath>
 
-#define SET_PERCISION(x) std::cout << std::fixed << std::setprecision(x);
+#define SET_PERCISION(x) std::cout << std::fixed << std::setprecision(x)
 #define INF std::numeric_limits<int>::max()
 #define PI M_PI
 
-std::array<std::array<char, 100>, 100> waffle;
-std::array<int, 100> sumH, sumV;
+#define GRID_MAX 100
+
+std::array<std::array<char, GRID_MAX + 1>, GRID_MAX + 1> waffle;
+std::array<std::array<int, GRID_MAX + 1>, GRID_MAX + 1> waffle_sum;
 
 void hacks() {
 	std::cout.sync_with_stdio(false);
@@ -34,100 +38,91 @@ void clean() {
 
 int main(int argc, char *argv[]) {
 	hacks();
+	int test_case_n = 0, test_case_c = 0;
 
-	int T, t_counter=0;
-
-	for(std::cin >> T; t_counter < T; t_counter ++){
+	for (std::cin >> test_case_n; test_case_c < test_case_n; test_case_c ++){
 		int R, C, H, V;
 		std::cin >> R >> C >> H >> V;
-		for (int i = 0; i < R; ++i) {
-			for (int j = 0; j < C; j++) {
-				std::cin >> waffle[i][j];
+
+		for (int i = 0; i <= R; ++i) {
+			for (int j = 0; j <= C; j++) {
+				waffle[i][j] = waffle_sum[i][j] = waffle_sum[i][j] = 0;
 			}
 		}
 
-		int totalSumV = 0;
-		sumV.fill(0);
-		for (int i = 0; i < R; ++i) {
-			for (int j = 0; j < C; j++) {
-				if (waffle[i][j] == '@') {
-					totalSumV ++;
+		int total_choco = 0;
+		for (int i = 1; i <= R; ++i) {
+			for (int j = 1; j <= C; j++) {
+				char &read = waffle[i][j];
+				while (read != '.' && read != '@') {
+					std::cin >> read;
+				}
+
+				waffle_sum[i][j] += waffle_sum[i - 1][j];
+				waffle_sum[i][j] += waffle_sum[i][j - 1];
+				waffle_sum[i][j] -= waffle_sum[i - 1][j - 1];
+
+				if (read == '@') {
+					waffle_sum[i][j] ++;
+					total_choco ++;
 				}
 			}
-			sumV[i + 1] = totalSumV;
+		}
+		std::cout << "Case #" << test_case_c + 1 << ": ";
+
+		int each_choco = total_choco / (H + 1) / (V + 1);
+		if (each_choco * (H + 1) * (V + 1) != total_choco) {
+			std::cout << "IMPOSSIBLE" << std::endl;
+			continue;
 		}
 
-		int totalSumH = 0;
-		sumH.fill(0);
-		for (int i = 0; i < C; ++i) {
-			for (int j = 0; j < R; j++) {
-				if (waffle[j][i] == '@') {
-					totalSumH ++;
-				}
-			}
-			sumH[i + 1] = totalSumH;
+		if (each_choco == 0) {
+			std::cout << "POSSIBLE" << std::endl;
+			continue;
 		}
 
-		std::vector<int> Hcuts(H + 1), Vcuts(V + 1);
-
-		int Vstep = totalSumV / (H + 1);
-		int Hstep = totalSumH / (V + 1);
-		if (totalSumH == 0 && totalSumV == 0) {
-			std::cout << "Case #" << t_counter + 1 << ": " << "POSSIBLE" << std::endl;
-		} else if (H > 0 && V > 0 && totalSumV % (H + 1) == 0 && totalSumH % (V + 1) == 0 && Vstep > 0 && Hstep > 0) {
-			int i = 0;
-			int flag = true;
-			for (int step = 0; step <= totalSumV; step += Vstep) {
-				for (; i <= R; ++i) {
-					if (sumV[i] == step) {
-						Hcuts.push_back(i);
-						break;
-					}
-				}
+		std::vector<int> V_cut = {0};
+		for (int i = 1; i <= C; ++i) {
+			if (waffle_sum[R][i] - waffle_sum[R][V_cut.back()] == total_choco / (V + 1)) {
+				V_cut.push_back(i);
 			}
-			if (i > R) {
-				flag = false;
-			}
-
-			i = 0;
-			for (int step = 0; step <= totalSumH; step += Hstep) {
-				for (; i <= C; ++i) {
-					if (sumH[i] == step) {
-						Vcuts.push_back(i);
-						break;
-					}
-				}
-			}
-			if (i > C) {
-				flag = false;
-			}
-
-			int count = -1;
-			for (int i = 1; i < Hcuts.size(); ++i) {
-				for (int j = 1; j < Vcuts.size(); ++j) {
-					int _count = 0;
-					for (int r = Hcuts[i - 1];  r < Hcuts[i]; ++r) {
-						for (int c = Vcuts[i - 1]; c < Vcuts[i]; ++c) {
-							if (waffle[i][j] == '@') {
-								_count ++;
-							}
-						}
-					}
-					if (count == -1) {
-						count = _count;
-					} else if (count != _count) {
-						flag = false;
-						break;
-					}
-				}
-			}
-			if (flag) {
-				std::cout << "Case #" << t_counter + 1 << ": " << "POSSIBLE" << std::endl;
-			} else {
-				std::cout << "Case #" << t_counter + 1 << ": " << "IMPOSSIBLE" << std::endl;
-			}
-		} else {
-			std::cout << "Case #" << t_counter + 1 << ": " << "IMPOSSIBLE" << std::endl;
 		}
+
+		if (V_cut.size() != V + 2) {
+			std::cout << "IMPOSSIBLE" << std::endl;
+			continue;
+		}
+
+		std::vector<int> H_cut = {0};
+		for (int i = 1; i <= R; ++i) {
+			if (waffle_sum[i][C] - waffle_sum[H_cut.back()][C] == total_choco / (H + 1)) {
+				H_cut.push_back(i);
+			}
+		}
+
+		if (H_cut.size() != H + 2) {
+			std::cout << "IMPOSSIBLE" << std::endl;
+			continue;
+		}
+
+		int flag = 0;
+		for (int i = 1; i < H_cut.size() && !flag; ++i) {
+			for (int j = 1; j < V_cut.size() && !flag; j++) {
+				int h = H_cut[i];
+				int v = V_cut[j];
+				int ph = H_cut[i - 1];
+				int pv = V_cut[j - 1];
+				if (waffle_sum[h][v] + waffle_sum[ph][pv] - waffle_sum[h][pv] - waffle_sum[ph][v] != each_choco) {
+					std::cout << "IMPOSSIBLE" << std::endl;
+					flag = 1;
+				}
+			}
+		}
+		if (flag) {
+			continue;
+		}
+
+		std::cout << "POSSIBLE" << std::endl;
 	}
+	return 0;
 }
